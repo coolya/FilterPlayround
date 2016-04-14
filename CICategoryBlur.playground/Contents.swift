d06e876d45b0486370c508cc0ab47a0e76065b95
Kolja Dummann <kolja.dummann@logv.ws>
2016-04-14 16:35:36 +0200

    separate playgrounds for categories

--- CICategoryBlur.playground/Contents.swift
+++ CICategoryBlur.playground/Contents.swift
@@ -31,6 +31,10 @@ func getResImg(name: String, ext: String = "jpg") -> CIImage {
 // The original work can be found at https://www.flickr.com/photos/ana_cotta/3077623496/
 let img = getResImg("sample")
 
+// ###############################################################################################
+// CICategoryBlur
+// ###############################################################################################
+
 
 // CIBoxBlur
 // https://developer.apple.com/library/mac/documentation/GraphicsImaging/Reference/CoreImageFilterReference/index.html#//apple_ref/doc/filter/ci/CIBoxBlur
@@ -125,6 +129,59 @@ let zoomBlurParams : [String : AnyObject] = [kCIInputImageKey: img,
 let zoomBlur = CIFilter(name: "CIZoomBlur", withInputParameters: zoomBlurParams);
 let zoomBlurOutput = zoomBlur?.outputImage
 
+
+// ###############################################################################################
+// CICategoryColorAdjustment
+// ###############################################################################################
+
+// CIColorClamp
+// https://developer.apple.com/library/mac/documentation/GraphicsImaging/Reference/CoreImageFilterReference/index.html#//apple_ref/doc/filter/ci/CIColorClamp
+
+let colorClampParams : [String : AnyObject] = [kCIInputImageKey: img,
+                                               "inputMinComponents": CIVector(x: 0.0, y: 0.0, z: 0.0, w: 0.0),
+                                               "inputMaxComponents" : CIVector(x: 1.0, y: 1.0, z: 1.0, w: 1.0)]
+
+let colorClamp = CIFilter(name: "CIColorClamp", withInputParameters: colorClampParams);
+let colorClampOutput = colorClamp?.outputImage
+
+
+// CIColorControls
+// https://developer.apple.com/library/mac/documentation/GraphicsImaging/Reference/CoreImageFilterReference/index.html#//apple_ref/doc/filter/ci/CIColorControls
+
+let colorControlsParams : [String : AnyObject] = [kCIInputImageKey: img,
+                                               "inputSaturation" : 1.0,
+                                               "inputBrightness" : 0.1,
+                                               "inputContrast" : 1.0]
+
+let colorControls = CIFilter(name: "CIColorControls", withInputParameters: colorControlsParams);
+let colorControlsOutput = colorControls?.outputImage
+
+
+// CIColorMatrix
+// https://developer.apple.com/library/mac/documentation/GraphicsImaging/Reference/CoreImageFilterReference/index.html#//apple_ref/doc/filter/ci/CIColorMatrix
+
+let colorMatrixParams : [String : AnyObject] = [kCIInputImageKey: img,
+                                                  "inputRVector" : CIVector(x: 1.0, y: 0.0, z: 0.0, w: 0.0),
+                                                  "inputGVector" : CIVector(x: 0.0, y: 1.0, z: 0.0, w: 0.0),
+                                                  "inputBVector" : CIVector(x: 0.0, y: 0.0, z: 1.0, w: 0.0),
+                                                  "inputAVector" : CIVector(x: 0.0, y: 0.0, z: 0.0, w: 1.0),
+                                                  "inputBiasVector" : CIVector(x: 0.0, y: 0.0, z: 0.0, w: 0.0)]
+
+let colorMatrix = CIFilter(name: "CIColorMatrix", withInputParameters: colorMatrixParams);
+let colorMatrixOutput = colorMatrix?.outputImage
+
+
+// CIColorPolynomial
+// https://developer.apple.com/library/mac/documentation/GraphicsImaging/Reference/CoreImageFilterReference/index.html#//apple_ref/doc/filter/ci/CIColorPolynomial
+
+let colorPolynomialParams : [String : AnyObject] = [kCIInputImageKey: img,
+                                                "inputRedCoefficients" : CIVector(x: 0.0, y: 1.0, z: 0.0, w: 0.0),
+                                                "inputGreenCoefficients" : CIVector(x: 0.0, y: 1.0, z: 0.0, w: 0.0),
+                                                "inputBlueCoefficients" : CIVector(x: 0.0, y: 1.0, z: 0.0, w: 0.0),
+                                                "inputAlphaCoefficients" : CIVector(x: 0.0, y: 1.0, z: 0.0, w: 0.0)]
+
+let colorPolynomial = CIFilter(name: "CIColorPolynomial", withInputParameters: colorPolynomialParams);
+let colorPolynomialOutput = colorPolynomial?.outputImage
 
 
 
